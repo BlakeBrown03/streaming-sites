d89e12a34a4033ddffd6b8b656e2c03427a13452
BlakeBrown03 <blake.m.brown03@gmail.com>
2025-01-15 11:50:54 -0600

    Update .gitignore to exclude config.js and integrate dotenv for API key management in Home component

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,4 +1,9 @@
+import * as streamingAvailability from "streaming-availability";
+import dotenv
+
 export default function Home(props: any) {
+	const API_KEY = process.env.API_KEY;
+
 	return (
 		<div className="flex items-center justify-items-center">
 			<h1>Home</h1>
