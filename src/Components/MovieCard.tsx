7bdc2543bd2e0d8a65babee3a037a033281af2d1
BlakeBrown03 <blake.m.brown03@gmail.com>
2025-01-20 14:58:21 -0600

    Enhance Home component to fetch and display top Netflix movies using MovieCard component

--- src/Components/MovieCard.tsx
+++ src/Components/MovieCard.tsx
@@ -1,7 +1,10 @@
 export default function MovieCard(props: any) {
 	return (
-		<div>
-			<img src={props.movie.poster} alt={props.movie.title} />
+		<div className="">
+			<img
+				src={props.movie.imageSet.verticalPoster.w240}
+				alt={props.movie.title}
+			/>
 			<h2>{props.movie.title}</h2>
 			<p>{props.movie.plot}</p>
 		</div>
